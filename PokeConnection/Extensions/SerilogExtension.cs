8cc93508bba22f3910d7ef780e0cada69233394b
Athos Schrapett <athosschrapett@outlook.com>
2025-02-23 10:36:49 -0300

    feat: inclusão do elasticsearch para gerenciamento de logs

--- PokeConnection/Extensions/SerilogExtension.cs
+++ PokeConnection/Extensions/SerilogExtension.cs
@@ -1,4 +1,9 @@
-﻿using Serilog;
+﻿using Elastic.Channels;
+using Elastic.Ingest.Elasticsearch.DataStreams;
+using Elastic.Ingest.Elasticsearch;
+using Elastic.Serilog.Sinks;
+using Elastic.Transport;
+using Serilog;
 
 namespace PokeConnection.API.Extensions;
 
@@ -6,13 +11,36 @@ public static class SerilogExtension
 {
     public static IHostBuilder AddSerilog(this IHostBuilder hostBuilder)
     {
-        Log.Logger = new LoggerConfiguration()
-            .WriteTo.Console()
-            .CreateLogger();
         hostBuilder.ConfigureLogging((context, logging) =>
         {
+            var configuration = context.Configuration;
+
+            Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .Enrich.WithMachineName()
+            .WriteTo.Console()
+            .WriteTo.Elasticsearch([new Uri("https://localhost:9200")], opts =>
+            {
+                opts.DataStream = new DataStreamName("logs", "pokeconnection", "dev");
+                opts.BootstrapMethod = BootstrapMethod.Failure;
+                opts.ConfigureChannel = channelOpts =>
+                {
+                    channelOpts.BufferOptions = new BufferOptions
+                    {
+                        ExportMaxRetries = 3,
+                        InboundBufferMaxSize = 1000
+                    };
+                };
+            }, transport =>
+            {
+                transport.Authentication(new BasicAuthentication("elastic", "U24efQYc7904d9zcvh_j"));
+                transport.ServerCertificateValidationCallback((sender, certificate, chain, sslPolicyErrors) => true);
+            })
+            .CreateLogger();
+
             logging.AddSerilog();
         });
+
         return hostBuilder;
     }
 }
